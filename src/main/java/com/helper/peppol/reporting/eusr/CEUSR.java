46a470f48cbe818de2dd3fa117e8bbafc84cf4cb
Philip Helger <philip@helger.com>
2023-01-08 11:31:32 +0100

    Constant values

---
+++ src/main/java/com/helper/peppol/reporting/eusr/CEUSR.java
@@ -0,0 +1,21 @@
+package com.helper.peppol.reporting.eusr;
+
+import javax.annotation.concurrent.Immutable;
+
+/**
+ * Constants for EUSR
+ *
+ * @author Philip Helger
+ */
+@Immutable
+public final class CEUSR
+{
+  /** The v1.0 Customization ID to be used */
+  public static final String CUSTOMIZATION_ID_V10 = "urn:fdc:peppol.eu:edec:trns:end-user-statistics-report:1.0";
+
+  /** The v1.0 Profile ID to be used */
+  public static final String PROFILE_ID_V10 = "urn:fdc:peppol.eu:edec:bis:reporting:1.0";
+
+  private CEUSR ()
+  {}
+}
---
+++ src/main/java/com/helper/peppol/reporting/tsr/CTSR.java
@@ -0,0 +1,21 @@
+package com.helper.peppol.reporting.tsr;
+
+import javax.annotation.concurrent.Immutable;
+
+/**
+ * Constants for TSR
+ *
+ * @author Philip Helger
+ */
+@Immutable
+public final class CTSR
+{
+  /** The v1.0 Customization ID to be used */
+  public static final String CUSTOMIZATION_ID_V10 = "urn:fdc:peppol.eu:edec:trns:transaction-statistics-reporting:1.0";
+
+  /** The v1.0 Profile ID to be used */
+  public static final String PROFILE_ID_V10 = "urn:fdc:peppol.eu:edec:bis:reporting:1.0";
+
+  private CTSR ()
+  {}
+}
