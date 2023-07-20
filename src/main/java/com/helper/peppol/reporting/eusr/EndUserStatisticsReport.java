d33f9d7410a68a86466494398b9baa98f55e020d
Philip Helger <philip@helger.com>
2023-07-20 21:20:39 +0200

    Added docs

--- src/main/java/com/helper/peppol/reporting/eusr/EndUserStatisticsReport.java
+++ src/main/java/com/helper/peppol/reporting/eusr/EndUserStatisticsReport.java
@@ -7,9 +7,13 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.helger.commons.builder.IBuilder;
 import com.helger.commons.datetime.OffsetDate;
 import com.helger.commons.datetime.XMLOffsetDate;
+import com.helger.commons.log.ConditionalLogger;
 import com.helger.commons.math.MathHelper;
 import com.helger.commons.string.StringHelper;
 import com.helger.peppol.reporting.jaxb.eusr.v110.EndUserStatisticsReportType;
@@ -19,6 +23,12 @@ import com.helger.peppol.reporting.jaxb.eusr.v110.ReportPeriodType;
 import com.helper.peppol.reporting.eusr.model.EUSRReportingItemList;
 import com.helper.peppol.reporting.model.PeppolReportingItem;
 
+/**
+ * Builder for Peppol End User Statistics Report objects.
+ *
+ * @author Philip Helger
+ * @since 1.2.0
+ */
 @Immutable
 public final class EndUserStatisticsReport
 {
@@ -27,14 +37,24 @@ public final class EndUserStatisticsReport
   private EndUserStatisticsReport ()
   {}
 
+  /**
+   * @return A new builder for EUSR 1.1 reports and never <code>null</code>.
+   */
   @Nonnull
-  public static Builder builder ()
+  public static Builder11 builder ()
   {
-    return new Builder ();
+    return new Builder11 ();
   }
 
-  public static final class Builder implements IBuilder <EndUserStatisticsReportType>
+  /**
+   * The main builder class for Peppol End User Statistics Reports v1.1.
+   *
+   * @author Philip Helger
+   */
+  public static class Builder11 implements IBuilder <EndUserStatisticsReportType>
   {
+    private static final Logger LOGGER = LoggerFactory.getLogger (EndUserStatisticsReport.Builder11.class);
+
     private String m_sCustomizationID;
     private String m_sProfileID;
     private LocalDate m_aStartDate;
@@ -43,131 +63,288 @@ public final class EndUserStatisticsReport
     private String m_sReportingServiceProviderID;
     private EUSRReportingItemList m_aList;
 
-    public Builder ()
+    /**
+     * Constructor. Sets default values for: {@link #customizationID(String)},
+     * {@link #profileID(String)} and
+     * {@link #reportingServiceProviderIDScheme(String)}
+     */
+    public Builder11 ()
     {
       customizationID (CEUSR.CUSTOMIZATION_ID_V11);
       profileID (CEUSR.PROFILE_ID_V10);
       reportingServiceProviderIDScheme (DEFAULT_REPORTER_ID_SCHEME);
     }
 
+    /**
+     * Set the customization ID to be used. Defaulted in the constructor.
+     *
+     * @param s
+     *        New value. May be <code>null</code>.
+     * @return this for chaining
+     */
     @Nonnull
-    public Builder customizationID (@Nullable final String s)
+    public Builder11 customizationID (@Nullable final String s)
     {
       m_sCustomizationID = s;
       return this;
     }
 
+    /**
+     * Set the profile ID to be used. Defaulted in the constructor.
+     *
+     * @param s
+     *        New value. May be <code>null</code>.
+     * @return this for chaining
+     */
     @Nonnull
-    public Builder profileID (@Nullable final String s)
+    public Builder11 profileID (@Nullable final String s)
     {
       m_sProfileID = s;
       return this;
     }
 
+    /**
+     * Set the reporting start date.
+     *
+     * @param a
+     *        New value. May be <code>null</code>.
+     * @return this for chaining
+     */
     @Nonnull
-    public Builder startDate (@Nullable final LocalDate a)
+    public Builder11 startDate (@Nullable final LocalDate a)
     {
       m_aStartDate = a;
       return this;
     }
 
+    /**
+     * Set the reporting end date.
+     *
+     * @param a
+     *        New value. May be <code>null</code>.
+     * @return this for chaining
+     */
     @Nonnull
-    public Builder endDate (@Nullable final LocalDate a)
+    public Builder11 endDate (@Nullable final LocalDate a)
     {
       m_aEndDate = a;
       return this;
     }
 
+    /**
+     * Set the reporting start and end date.
+     *
+     * @param a
+     *        The date from which the first day of the month and the last day of
+     *        the month are taken. May be <code>null</code>.
+     * @return this for chaining
+     * @see #startDate(LocalDate)
+     * @see #endDate(LocalDate)
+     */
     @Nonnull
-    public Builder monthOf (@Nullable final LocalDate a)
+    public Builder11 monthOf (@Nullable final LocalDate a)
     {
-      return startDate (a == null ? null : a.withDayOfMonth (1)).endDate (a == null ? null : a.plusMonths (1)
-                                                                                              .withDayOfMonth (1)
-                                                                                              .minusDays (1));
+      return startDate (a == null ? null : a.withDayOfMonth (1)).endDate (a == null ? null
+                                                                                    : a.plusMonths (1)
+                                                                                       .withDayOfMonth (1)
+                                                                                       .minusDays (1));
     }
 
+    /**
+     * Set the reporting start and end date.
+     *
+     * @param a
+     *        The date from which the first day of the month and the last day of
+     *        the month are taken. May be <code>null</code>.
+     * @return this for chaining
+     * @see #startDate(LocalDate)
+     * @see #endDate(LocalDate)
+     */
     @Nonnull
-    public Builder monthOf (@Nullable final OffsetDate a)
+    public Builder11 monthOf (@Nullable final OffsetDate a)
     {
       return monthOf (a == null ? null : a.toLocalDate ());
     }
 
+    /**
+     * Set the reporting start and end date.
+     *
+     * @param a
+     *        The date from which the first day of the month and the last day of
+     *        the month are taken. May be <code>null</code>.
+     * @return this for chaining
+     * @see #startDate(LocalDate)
+     * @see #endDate(LocalDate)
+     */
     @Nonnull
-    public Builder monthOf (@Nullable final OffsetDateTime a)
+    public Builder11 monthOf (@Nullable final OffsetDateTime a)
     {
       return monthOf (a == null ? null : a.toLocalDate ());
     }
 
+    /**
+     * Set the reporting start and end date.
+     *
+     * @param a
+     *        The date from which the first day of the month and the last day of
+     *        the month are taken. May be <code>null</code>.
+     * @return this for chaining
+     * @see #startDate(LocalDate)
+     * @see #endDate(LocalDate)
+     */
     @Nonnull
-    public Builder monthOf (@Nullable final XMLOffsetDate a)
+    public Builder11 monthOf (@Nullable final XMLOffsetDate a)
     {
       return monthOf (a == null ? null : a.toLocalDate ());
     }
 
+    /**
+     * Set the reporting Service Provider ID scheme to be used. Defaulted in the
+     * constructor.
+     *
+     * @param s
+     *        New value. May be <code>null</code>.
+     * @return this for chaining
+     */
     @Nonnull
-    public Builder reportingServiceProviderIDScheme (@Nullable final String s)
+    public Builder11 reportingServiceProviderIDScheme (@Nullable final String s)
     {
       m_sReportingServiceProviderIDScheme = s;
       return this;
     }
 
+    /**
+     * Set the reporting Service Provider ID to be used. Usually has the layout
+     * <code>P[A-Z]{2}[0-9]{6}</code>.
+     *
+     * @param s
+     *        New value. May be <code>null</code>.
+     * @return this for chaining
+     */
     @Nonnull
-    public Builder reportingServiceProviderID (@Nullable final String s)
+    public Builder11 reportingServiceProviderID (@Nullable final String s)
     {
       m_sReportingServiceProviderID = s;
       return this;
     }
 
+    /**
+     * Set the EUSR reporting items based on which the report is to be created.
+     *
+     * @param a
+     *        The list to be set. May be <code>null</code>.
+     * @return this for chaining
+     */
     @Nonnull
-    public Builder reportingItemList (@Nullable final EUSRReportingItemList a)
+    public Builder11 reportingItemList (@Nullable final EUSRReportingItemList a)
     {
       m_aList = a;
       return this;
     }
 
+    /**
+     * Set the EUSR reporting items based on which the report is to be created.
+     *
+     * @param aItems
+     *        The items of which a new {@link EUSRReportingItemList} is created
+     *        and used. May be <code>null</code>.
+     * @return this for chaining
+     */
     @Nonnull
-    public Builder reportingItemList (@Nullable final PeppolReportingItem... aItems)
+    public Builder11 reportingItemList (@Nullable final PeppolReportingItem... aItems)
     {
       return reportingItemList (aItems == null ? null : new EUSRReportingItemList (aItems));
     }
 
+    /**
+     * Set the EUSR reporting items based on which the report is to be created.
+     *
+     * @param aItems
+     *        The items of which a new {@link EUSRReportingItemList} is created
+     *        and used. May be <code>null</code>.
+     * @return this for chaining
+     */
     @Nonnull
-    public Builder reportingItemList (@Nullable final Iterable <? extends PeppolReportingItem> aItems)
+    public Builder11 reportingItemList (@Nullable final Iterable <? extends PeppolReportingItem> aItems)
     {
       return reportingItemList (aItems == null ? null : new EUSRReportingItemList (aItems));
     }
 
-    public boolean isComplete ()
+    /**
+     * Check if all mandatory fields are set or not.
+     *
+     * @param bLogFailures
+     *        <code>true</code> if missing fields should be logged,
+     *        <code>false</code> if not.
+     * @return <code>true</code> if all mandatory fields are set,
+     *         <code>false</code> if not.
+     */
+    public boolean isComplete (final boolean bLogFailures)
     {
+      final ConditionalLogger aCondLogger = new ConditionalLogger (LOGGER, bLogFailures);
       if (StringHelper.hasNoText (m_sCustomizationID))
+      {
+        aCondLogger.warn ("CustomizationID is missing");
         return false;
+      }
 
       if (StringHelper.hasNoText (m_sProfileID))
+      {
+        aCondLogger.warn ("ProfileID is missing");
         return false;
+      }
 
       if (m_aStartDate == null)
+      {
+        aCondLogger.warn ("StartDate is missing");
         return false;
+      }
 
       if (m_aEndDate == null)
+      {
+        aCondLogger.warn ("EndDate is missing");
         return false;
+      }
+
+      if (m_aEndDate.isBefore (m_aStartDate))
+      {
+        aCondLogger.warn ("StartDate must be before or equal to the EndDate");
+        return false;
+      }
 
       if (StringHelper.hasNoText (m_sReportingServiceProviderIDScheme))
+      {
+        aCondLogger.warn ("Reporting Service Provider ID Scheme is missing");
         return false;
+      }
 
       if (StringHelper.hasNoText (m_sReportingServiceProviderID))
+      {
+        aCondLogger.warn ("Reporting Service Provider ID is missing");
         return false;
+      }
 
       if (m_aList == null)
+      {
+        aCondLogger.warn ("Reporting Item list is missing");
         return false;
+      }
 
+      aCondLogger.trace ( () -> "Builder fields are complete");
       return true;
     }
 
+    /**
+     * Build the main EUSR report. Use {@link #isComplete(boolean)} to check if
+     * all mandatory fields are set or not.
+     *
+     * @see #isComplete(boolean)
+     */
     @Nonnull
     public EndUserStatisticsReportType build ()
     {
-      if (!isComplete ())
-        throw new IllegalStateException ("The builder was not filled completely");
+      if (!isComplete (true))
+        throw new IllegalStateException ("The EUSR builder was not filled completely");
 
       final EndUserStatisticsReportType aReport = new EndUserStatisticsReportType ();
       aReport.setCustomizationID (m_sCustomizationID);
