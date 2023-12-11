2e574ebf2fe311613d2e51195674ce2983a1fef0
Philip Helger <philip@helger.com>
2023-12-11 21:21:01 +0100

    Removed license headers

--- peppol-reporting/src/test/java/com/helper/peppol/reporting/tsr/model/FuncTestCreateTSR.java
+++ peppol-reporting/src/test/java/com/helper/peppol/reporting/tsr/model/FuncTestCreateTSR.java
@@ -20,7 +20,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.math.BigInteger;
+import java.time.Month;
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.function.IntFunction;
 
 import org.junit.Test;
@@ -38,6 +40,7 @@ import com.helger.peppolid.peppol.process.EPredefinedProcessIdentifier;
 import com.helger.schematron.svrl.SVRLHelper;
 import com.helger.schematron.svrl.jaxb.SchematronOutputType;
 import com.helper.peppol.reporting.api.CPeppolReporting;
+import com.helper.peppol.reporting.api.EReportingDirection;
 import com.helper.peppol.reporting.api.PeppolReportingItem;
 import com.helper.peppol.reporting.tsr.TransactionStatisticsReport;
 import com.helper.peppol.reporting.tsr.TransactionStatisticsReport101Marshaller;
@@ -46,7 +49,7 @@ import com.helper.peppol.reporting.tsr.TransactionStatisticsReportValidator;
 public final class FuncTestCreateTSR
 {
   private static final Logger LOGGER = LoggerFactory.getLogger (FuncTestCreateTSR.class);
-  private static final String MY_SPID = "PDE000001";
+  private static final String MY_SPID = "POP000360";
 
   @Test
   public void testCreateEmpty () throws Exception
@@ -355,6 +358,115 @@ public final class FuncTestCreateTSR
                          .stream ()
                          .filter (x -> x.getType ().equals (SubtotalKeySP_DT_PR_CC.TYPE))
                          .count ());
+
+    // Avoid bloating the logs
+    if (false)
+      LOGGER.info (new TransactionStatisticsReport101Marshaller ().setFormattedOutput (true).getAsString (aReport));
+
+    // Ensure it is valid XML
+    final Document aDoc = new TransactionStatisticsReport101Marshaller ().getAsDocument (aReport);
+    assertNotNull (aDoc);
+
+    // Perform Schematron verification
+    final SchematronOutputType aSVRL = TransactionStatisticsReportValidator.getSchematronTSR_10 ()
+                                                                           .applySchematronValidationToSVRL (aDoc,
+                                                                                                             null);
+    assertEquals (0, SVRLHelper.getAllFailedAssertionsAndSuccessfulReports (aSVRL).size ());
+  }
+
+  @Test
+  public void testCreateExampleForSpec () throws Exception
+  {
+    final OffsetDateTime aNow = PDTFactory.createLocalDate (2023, Month.DECEMBER, 1)
+                                          .atStartOfDay ()
+                                          .atOffset (ZoneOffset.UTC);
+    final String sOtherSPID1 = "POP000001";
+    final String sOtherSPID2 = "POP000002";
+    final String sEndUserID = "abc";
+
+    final ICommonsList <PeppolReportingItem> aItems = new CommonsArrayList <> ();
+    for (int i = 0; i < 4; ++i)
+    {
+      aItems.add (PeppolReportingItem.builder ()
+                                     .exchangeDateTime (aNow)
+                                     .directionReceiving ()
+                                     .c2ID (sOtherSPID1)
+                                     .c3ID (MY_SPID)
+                                     .docTypeID (EPredefinedDocumentTypeIdentifier.INVOICE_EN16931_PEPPOL_V30)
+                                     .processID (EPredefinedProcessIdentifier.BIS3_BILLING)
+                                     .transportProtocolPeppolAS4v2 ()
+                                     .c1CountryCode ("DE")
+                                     .c4CountryCode ("AT")
+                                     .endUserID (sEndUserID)
+                                     .build ());
+    }
+    for (int i = 0; i < 20; ++i)
+    {
+      final int nCut = 11;
+      aItems.add (PeppolReportingItem.builder ()
+                                     .exchangeDateTime (aNow)
+                                     .direction (i < nCut ? EReportingDirection.SENDING : EReportingDirection.RECEIVING)
+                                     .c2ID (i < nCut ? MY_SPID : sOtherSPID1)
+                                     .c3ID (i < nCut ? sOtherSPID1 : MY_SPID)
+                                     .docTypeID (EPredefinedDocumentTypeIdentifier.INVOICE_EN16931_PEPPOL_V30)
+                                     .processID (EPredefinedProcessIdentifier.BIS3_BILLING)
+                                     .transportProtocolPeppolAS4v2 ()
+                                     .c1CountryCode ("DE")
+                                     .c4CountryCode (i < nCut ? null : "DE")
+                                     .endUserID (sEndUserID)
+                                     .build ());
+    }
+    for (int i = 0; i < 2; ++i)
+    {
+      aItems.add (PeppolReportingItem.builder ()
+                                     .exchangeDateTime (aNow)
+                                     .directionReceiving ()
+                                     .c2ID (sOtherSPID1)
+                                     .c3ID (MY_SPID)
+                                     .docTypeID (EPredefinedDocumentTypeIdentifier.CREDITNOTE_EN16931_PEPPOL_V30)
+                                     .processID (EPredefinedProcessIdentifier.BIS3_BILLING)
+                                     .transportProtocolPeppolAS4v2 ()
+                                     .c1CountryCode ("BE")
+                                     .c4CountryCode ("DE")
+                                     .endUserID (sEndUserID)
+                                     .build ());
+    }
+    for (int i = 0; i < 12; ++i)
+    {
+      aItems.add (PeppolReportingItem.builder ()
+                                     .exchangeDateTime (aNow)
+                                     .directionSending ()
+                                     .c2ID (MY_SPID)
+                                     .c3ID (sOtherSPID2)
+                                     .docTypeID (EPredefinedDocumentTypeIdentifier.ORDER_FDC_PEPPOL_EU_POACC_TRNS_ORDER_3)
+                                     .processID (EPredefinedProcessIdentifier.BIS3_ORDERING)
+                                     .transportProtocolPeppolAS4v2 ()
+                                     .c1CountryCode ("BE")
+                                     .endUserID (sEndUserID)
+                                     .build ());
+    }
+    for (int i = 0; i < 12; ++i)
+    {
+      aItems.add (PeppolReportingItem.builder ()
+                                     .exchangeDateTime (aNow)
+                                     .directionReceiving ()
+                                     .c2ID (sOtherSPID1)
+                                     .c3ID (MY_SPID)
+                                     .docTypeID (EPredefinedDocumentTypeIdentifier.ORDERRESPONSE_FDC_PEPPOL_EU_POACC_TRNS_ORDER_RESPONSE_3)
+                                     .processID (EPredefinedProcessIdentifier.BIS3_ORDERING)
+                                     .transportProtocolPeppolAS4v2 ()
+                                     .c1CountryCode ("DE")
+                                     .c4CountryCode (i < 7 ? "AT" : "DE")
+                                     .endUserID (sEndUserID)
+                                     .build ());
+    }
+
+    // Create report with exactly one transaction
+    final TransactionStatisticsReportType aReport = TransactionStatisticsReport.builder ()
+                                                                               .monthOf (aNow)
+                                                                               .reportingServiceProviderID (MY_SPID)
+                                                                               .reportingItemList (aItems)
+                                                                               .build ();
 
     // Avoid bloating the logs
     if (false)
